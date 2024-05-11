e64e88cf1a382a5811f68c7e61786543665dc6bf
Victor Javier Vidal Durón <79221424+VictorVidal7@users.noreply.github.com>
2024-05-10 23:42:53 -0600

    Update twitter_api_connector_v2.py

--- twitter_api_connector_v2.py
+++ twitter_api_connector_v2.py
@@ -1,13 +1,13 @@
 import tweepy
 
 # Your keys and tokens
-consumer_key = 'Pu3GdjAoClNhHHFNqgIqjLATH'
-consumer_secret = 'xvNfymeHyzvUA1eElYOYLjwNvdeTWjcUqRYY9SRQbfpdekebHo'
-access_token = '1768031798129295361-5TRykIlOToNTWEqH3HHZgwjIqemTZ1'
-access_token_secret = 'fEfjGpR2ev9UYAmpljd9Xgmg2Mdk95uadBRV1T4SpQYCh'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 # Autenticación con OAuth 2.0
-client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAADn4tgEAAAAALU%2FQl9iJ1Dvivl1qDvz9EsAebvg%3DNO6VlaJtgNEZHbvGxVnp3IEfLysCjCQDOccymbmrou0P2QpZ7d')
+client = tweepy.Client(bearer_token='')
 
 # Realizar una búsqueda utilizando la API v2
 query = 'tecnología lang:es'
